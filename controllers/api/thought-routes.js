c978645dc21699fcd135f6cbad8f02fcb77d022a
Alejandra <alejandra.e.bernabel@gmail.com>
2023-11-22 15:07:35 -0500

    Began working on thought routes

--- controllers/api/thought-routes.js
+++ controllers/api/thought-routes.js
@@ -1,23 +1,78 @@
+const Thought = require('../../models/Thought');
 const router = require ('express').Router();
 
 // /api/thoughts routes
 
 //get all thoughts
-
+router.get('/', async (req, res) => {
+    try {
+        const thought = await Thought.find();
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //get one thought by id
+router.get('/:id', async (req, res) => {
+    try {
+        const thought = await Thought.findOne({_id: req.params.id})
 
+        if (!thought) {
+            return res.status(404).json({message: 'No thought with that ID exists'});
+        }
+
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //post a new thought
-
+router.post('/', async (req, res) => {
+    try {
+        const thoughtData = await Thought.create(req.body);
+        res.json(thoughtData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 // update a thougth by id
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedThought = await Thought.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        );
 
+        if (!updatedThought) {
+            return res.status(404).json({message: 'No such thought exists'});
+        };
+
+        res.json(updatedThought);
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 // delete a thought by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const thought = await Thought.deleteOne({_id: req.params.id});
 
-
-
+        if (!thought) {
+            return res.status(404).json({message: 'No such thought exists'});
+        };
+    
+        res.json({message: 'Thought successfully deleted'});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    
+});
 
 
 
